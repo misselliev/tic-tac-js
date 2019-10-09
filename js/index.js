19b7742de6b45551381f91bdcbf4f75da1afa138
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-10-09 13:29:06 -0700

    linting fixes

--- js/index.js
+++ js/index.js
@@ -6,29 +6,27 @@ class Player {
     this.score = 0;
     this.mark = mark;
   }
-  
+
   getPlayerName = () => {
     return this.name;
-  }
+  };
 
   getPlayerMark = () => {
     return this.mark;
-  }
+  };
 
   getPlayerMoves = () => {
     return this.moves;
-  }
+  };
 
   setPlayerMoves = val => {
     return this.moves.push(parseInt(val, 10));
-  }
+  };
 
   getPlayerScore = () => {
     return this.score;
-  }
-
+  };
 }
-
 const streaks = [
   [1, 2, 3],
   [4, 5, 6],
@@ -37,7 +35,7 @@ const streaks = [
   [2, 5, 8],
   [3, 6, 9],
   [1, 5, 9],
-  [3, 5, 7],
+  [3, 5, 7]
 ];
 const human = new Player('Human', 'X');
 const computer = new Player('Computer', '0');
@@ -46,7 +44,7 @@ Array.prototype.sample = function() {
   return this[Math.floor(Math.random() * this.length)];
 };
 
-const checkWinner = (player) => {
+const checkWinner = player => {
   if (player.moves.length >= 3) {
     for (let i = 0; i < streaks.length; i += 1) {
       const moves = player.getPlayerMoves();
@@ -54,10 +52,14 @@ const checkWinner = (player) => {
       if (line.length === 3) {
         player.score += 1;
         if (player.name === 'Human') {
-          document.getElementById('human_score').innerHTML = `Your score : ${player.score}`;
+          document.getElementById(
+            'human_score'
+          ).innerHTML = `Your score : ${player.score}`;
           document.getElementById('round_score').innerHTML = 'You win!';
         } else {
-          document.getElementById('computer_score').innerHTML = `Computer score : ${player.score}`;
+          document.getElementById(
+            'computer_score'
+          ).innerHTML = `Computer score : ${player.score}`;
           document.getElementById('round_score').innerHTML = 'Computer wins!';
         }
         return true;
@@ -92,9 +94,9 @@ const Board = () => {
   table.classList = 'ui celled table';
   boardgame.appendChild(table);
 
-  initMoves.forEach((items) => {
+  initMoves.forEach(items => {
     const row = document.createElement('tr');
-    items.forEach((val) => {
+    items.forEach(val => {
       const cell = document.createElement('td');
       cell.setAttribute('id', val);
       cell.className = 'cell';
@@ -124,4 +126,3 @@ const Game = (() => {
   }
   return { turn, computersMove };
 })();
-
